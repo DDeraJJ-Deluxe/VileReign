f3cf794917adaaa44addd173ce68f31a333b93f3
Allan Chan <allanchan@cpp.edu>
2023-04-22 23:29:09 -0700

    Added PlayerHealth script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class EnemyController : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int damage;
+    public PlayerHealth playerHealth;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            playerHealth.TakeDamage(damage);
+        }
     }
 }
