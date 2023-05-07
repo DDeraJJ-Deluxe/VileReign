7d5f5a50b8806fc6f8c8216dfbc2fa0acad0cb0b
DDeraJJ-Deluxe <wownotevenbro@gmail.com>
2023-05-07 03:32:02 -0700

    Platform hell is over

---
+++ Assets/Scripts/Platforms.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    private float waitTime = 0.5f;  
+    void Start() {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+    
+    void Update() {
+        if (Input.GetKey(KeyCode.S)) {
+            effector.rotationalOffset = 180f;
+            Invoke("ResetPlatformEffector", waitTime);
+        }
+    }
+
+    void ResetPlatformEffector() {
+        effector.rotationalOffset = 0f;
+    }
+}
