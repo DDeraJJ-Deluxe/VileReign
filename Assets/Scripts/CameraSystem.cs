7c717234c4c59348aa8ef0c78fd8322dca02d952
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2023-05-08 01:42:39 -0700

    scene transition

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -6,8 +6,8 @@ public class CameraSystem : MonoBehaviour {
     private GameObject player;
     public float xMin;
     public float xMax;
-    //public float yMin;
-    //public float yMax;
+    public float yMin;
+    public float yMax;
 
     // Start is called before the first frame update
     void Start() {
@@ -17,7 +17,7 @@ public class CameraSystem : MonoBehaviour {
     // Update is called once per frame
     void LateUpdate() {
         float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
-        //float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
-        gameObject.transform.position = new Vector3(x, 0f, -10);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, -10);
     }
 }
