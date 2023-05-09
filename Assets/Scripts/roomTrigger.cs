9d0a2a3cd7c0d6f34679b19feb7e7548bad29f3a
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2023-05-09 01:02:26 -0700

    Camera switch feature for each room

--- Assets/Scripts/roomTrigger.cs
+++ Assets/Scripts/roomTrigger.cs
@@ -2,36 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
  
 public class roomTrigger : MonoBehaviour
 {
-    public GameObject virtualCam;
+    public CinemachineVirtualCamera virtualCam;
  
-    // Level move zoned enter, if collider is a player
-    // Move game to another scene
     private void OnTriggerEnter2D(Collider2D other) {
         print("Trigger Entered");
         
-        // Could use other.GetComponent<Player>() to see if the game object has a Player component
-        // Tags work too. Maybe some players have different script components?
-        if(other.tag == "Player" && !other.isTrigger) {
-            // Player entered, so move level
-            //print("Switching Scene to " + sceneBuildIndex);
-            //SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-            virtualCam.SetActive(true);
+        if(other.CompareTag("Player") && !other.isTrigger) {
+            virtualCam = GetComponentInChildren<CinemachineVirtualCamera>();
+            virtualCam.Priority = 10;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        print("Trigger Entered");
+        print("Trigger Exited");
         
-        // Could use other.GetComponent<Player>() to see if the game object has a Player component
-        // Tags work too. Maybe some players have different script components?
-        if(other.tag == "Player" && !other.isTrigger) {
-            // Player entered, so move level
-            //print("Switching Scene to " + sceneBuildIndex);
-            //SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-            virtualCam.SetActive(false);
+        if(other.CompareTag("Player") && !other.isTrigger) {
+            virtualCam = GetComponentInChildren<CinemachineVirtualCamera>();
+            virtualCam.Priority = -1;
         }
     }
 }
